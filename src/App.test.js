038c5af2320bfc94e38a8d542c9d93515b980d36
orba.abel <abel.orban89@gmail.com>
2019-09-21 14:21:19 +0200

    test2

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import {shallow} from "enzyme";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+// 1. Test suite
+describe("[UNIT] Testing the App component", () => {
+  let wrapper;
+
+  // 2. A Jest setup helper function
+  beforeEach(() => {
+    // 3. Enzyme's shallow rendering
+    wrapper = shallow(<App/>)
+  });
+
+  describe("Component validation", () => {
+    // 4. Our unit test, checking if the initial value is 0
+    it('displays 0 as a default value', () => {
+      expect(wrapper.find("h1").text()).toContain("0")
+    });
+
+    it("should increase counter when the button is clicked", () => {
+      wrapper.find("button").simulate("click");
+      expect(wrapper.find("h1").text()).toContain("1")
+    });
+  })
 });
